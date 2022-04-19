746be130ab25fa3a7f16870f3404389acd40630e
codervivek496 <viveksinghssm496@gmail.com>
2022-04-19 08:42:11 +0530

    uc2

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,8 +1,17 @@
 # Flip Coin Simulation
-flipCion=$(( RANDOM%2 ))
-if [ $flipCoin -eq 0 ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+h=0
+t=0
+
+
+for (( i=0; i<1000; i++ ))
+do
+	flip=$(( RANDOM%2 ))
+	if [ $flip -eq 0 ]
+	then
+		h=$((h+1))
+	else
+		t=$((t+1))
+	fi
+done
+echo "Times head was flipped: " $h
+echo "Times tail was flipped: " $t
